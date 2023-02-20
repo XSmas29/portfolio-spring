0482573d0887b6069cbabd5fb3fc11b47c7acd6d
Surya Bumantara <bumantara.surya2213@gmail.com>
2023-02-20 20:42:31 +0700

    init spring app

---
+++ src/test/java/id/my/bumantara/portfoliospring/PortfolioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.bumantara.portfoliospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
